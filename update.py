385ae19c366b4f915c21ba34f790726d396f7666
resuther <72550915+resuther@users.noreply.github.com>
2022-03-27 15:43:46 +1300

    Updates Test.txt and pulls API updates.

--- update.py
+++ update.py
@@ -7,37 +7,27 @@ def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-time.time()
 response = requests.get("https://api.sunrise-sunset.org/json?lat=-36.111413&lng=174.585372&date=today")
 s = response.json()['results']['sunset']
 e = response.json()['results']['astronomical_twilight_end']
-
 start = s[:7]
-endedit = e[:7]
-endeditt = endedit[:1]
-endedittt = int(endeditt)
-endeditttt = (f"{endedittt + 1}")
-
-sky = 0
-
-def manual_replace(s, char, index):
-    return s[:index] + char + s[index +1:]
-
-string = endedit
-end = (manual_replace(string, endeditttt, 0))
+end = e[:7]
+yn = 0
 
 while True:
-    text = (f"{datetime.utcnow()}")
-    textt = text[12:]
-    utctime = textt[:7]
-    if utctime != start:
-        if utctime != endedit:
-            if sky == 0:
+    # defines time \/
+    nowww = datetime.now()
+    noww = nowww.strftime('%I:%M:%S')
+    now = noww[1:]
+    
+    if now != start:
+        if now != end:
+            if yn == 0:
                 with open('Test.txt', 'w') as file:
                     file.write("")
-    if utctime == start:
+    if now == start:
         with open('Test.txt', 'w') as file:
             file.write(".")
-            sky = 1
-    if utctime == endedit:
-        sky = 0
+            yn = 1
+    if now == end:
+        yn = 0
